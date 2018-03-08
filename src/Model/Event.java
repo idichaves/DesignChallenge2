1710d7db2a428026a04499b8134fe53ed3c839dc
dich <34095473+idichaves@users.noreply.github.com>
2018-03-08 23:09:31 +0800

    edited model classes

--- src/Model/Event.java
+++ src/Model/Event.java
@@ -3,15 +3,20 @@ package model;
 public class Event extends CalendarItem {
     private boolean holidayIndicator;
 
-    public Event(String date, int hrStart, int minStart, int hrEnd, int minEnd, String name, String color, boolean holiday) {
+    public Event(String date, int hrStart, int minStart, String timeStart, String timeEnd, String name, String color, boolean holiday) {
         String[] dateComps = date.split("/");
         super.setMonth(Integer.parseInt(dateComps[0]));
         super.setDay(Integer.parseInt(dateComps[1]));
         super.setYear(Integer.parseInt(dateComps[2]));
-        super.setHrStart(hrStart);
-        super.setMinStart(minStart);
-        super.setHrEnd(hrEnd);
-        super.setMinEnd(minEnd);
+
+        String[] timeComps = timeStart.split(":");
+        super.setHrStart(Integer.parseInt(timeComps[0]));
+        super.setMinStart(Integer.parseInt(timeComps[1]));
+
+        String[] timeEndComps = timeEnd.split(":");
+        super.setHrEnd(Integer.parseInt(timeEndComps[0]));
+        super.setMinEnd(Integer.parseInt(timeEndComps[1]));
+
         super.setName(name);
         super.setColor(color);
         holidayIndicator = holiday;
--- src/Model/ToDo.java
+++ src/Model/ToDo.java
@@ -3,14 +3,20 @@ package model;
 public class ToDo extends CalendarItem {
     private boolean accomplished;
 
-    public ToDo(int month, int day, int year, int hrStart, int minStart, int hrEnd, int minEnd, String name, String color) {
-        super.setMonth(month);
-        super.setDay(day);
-        super.setYear(year);
-        super.setHrStart(hrStart);
-        super.setMinStart(minStart);
-        super.setHrEnd(hrEnd);
-        super.setMinEnd(minEnd);
+    public ToDo(String date, String timeStart, String timeEnd, String name, String color) {
+        String[] dateComps = date.split("/");
+        super.setMonth(Integer.parseInt(dateComps[0]));
+        super.setDay(Integer.parseInt(dateComps[1]));
+        super.setYear(Integer.parseInt(dateComps[2]));
+
+        String[] timeComps = timeStart.split(":");
+        super.setHrStart(Integer.parseInt(timeComps[0]));
+        super.setMinStart(Integer.parseInt(timeComps[1]));
+
+        String[] timeEndComps = timeEnd.split(":");
+        super.setHrEnd(Integer.parseInt(timeEndComps[0]));
+        super.setMinEnd(Integer.parseInt(timeEndComps[1]));
+
         super.setName(name);
         super.setColor(color);
         accomplished = false;
