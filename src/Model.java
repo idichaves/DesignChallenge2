d33b773b23683a49c55069e6202b27cdb5911473
Shadrachp <shadrachgabriel@icloud.com>
2018-03-09 23:20:49 +0800

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- src/Model.java
+++ src/Model.java
@@ -2,7 +2,7 @@ import model.*;
 
 
 import java.util.ArrayList;
-
+//Last Modified: 03/09/2018 - 11:01PM
 public class Model {
     private ArrayList<CalendarItem> calendarItems;
     private MonthViewModel monthViewModel;
@@ -12,11 +12,19 @@ public class Model {
     public Model(){
         calendarItems = new ArrayList<>();
         fileImporters = new ArrayList<>();
+        fileImporters.add(new CSVImport());
         dateLabelFormatter = new DateLabelFormatter();
         monthViewModel = new MonthViewModel();
     }
 
+    //gets content from file
     public ArrayList<CalendarItem> getCalendarItems() {
+        for (int i = 0; i < fileImporters.size(); i++) {
+           ArrayList<CalendarItem> temp =  fileImporters.get(i).getCalendarItems();
+            for (int j = 0; j < temp.size(); j++)
+                calendarItems.add(temp.get(j));
+        }
+
         return calendarItems;
     }
 
