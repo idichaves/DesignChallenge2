8a68fd8f26f9997e7bc00694a05ccce37b8863a9
Shadrachp <shadrachgabriel@icloud.com>
2018-03-10 22:16:18 +0800

    Shad

--- src/view/DayViewPanel.java
+++ src/view/DayViewPanel.java
@@ -21,15 +21,11 @@ public class DayViewPanel extends JPanel {
         lblToday.setBounds(30, 13, 204, 35);
         add(lblToday);
         tableModel = new DefaultTableModel(48, 2){
-            int i = 0;
             public boolean isCellEditable(int row, int col){
                 return false;
             }
-
         };
         tableModel.setColumnIdentifiers(new String[] {"Time", "Item"});
-<<<<<<< HEAD
-<<<<<<< HEAD
             for (int i = 0; i < 24; i++) {
                 int hr = i + 1;
                 if(hr <= 12) {
@@ -44,18 +40,6 @@ public class DayViewPanel extends JPanel {
             }
 
 
-
-=======
-=======
->>>>>>> 95ac90dae8816729f99509cf4efa3a233f35c535
-        
-        for (int i = 0; i < 24; i++) {
-            if(i+1 <= 12)
-                tableModel.setValueAt(i + 1 + ":00AM", i,0);
-            else
-                tableModel.setValueAt(i - 11 + ":00PM", i, 0);
-        }
->>>>>>> 95ac90dae8816729f99509cf4efa3a233f35c535
 
         dayTable = new JTable(tableModel);
         dayTable.setRowHeight(50);
